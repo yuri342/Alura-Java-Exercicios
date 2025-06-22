c3d181ac8cbad68da1ce09dbe8741ca296863ff7
yuri <97260704+yuri342@users.noreply.github.com>
2025-06-22 14:12:21 -0300

    Create Exercicio12.java

---
+++ Exercicio12.java
@@ -0,0 +1,11 @@
+public class ConversaoDoubleParaInt {
+
+    public static void main(String[] args) {
+        double valorDouble = 19.5;
+
+        // Conversão de double para int (truncamento)
+        int valorInteiro = (int) valorDouble;
+
+        System.out.println("O valor inteiro do produto é: " + valorInteiro);
+    }
+}
