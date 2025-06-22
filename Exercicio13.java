53614ec8921dfb180f3485bb0cbd1ae731785a4a
yuri <97260704+yuri342@users.noreply.github.com>
2025-06-22 14:12:54 -0300

    Create Exercicio13.java

---
+++ Exercicio13.java
@@ -0,0 +1,12 @@
+public class CalculadoraMedia {
+
+    public static void main(String[] args) {
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 9.0;
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
