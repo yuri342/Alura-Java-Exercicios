14bc79102cfefd56ed1642c8a9dbf9c7486be430
yuri <97260704+yuri342@users.noreply.github.com>
2025-06-22 14:13:29 -0300

    Create Exercicio15.java

---
+++ Exercicio15.java
@@ -0,0 +1,10 @@
+public class ConversaoTemperatura {
+
+    public static void main(String[] args) {
+        int celsius = 20;
+
+        double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+        System.out.println("A temperatura em graus Fahrenheit é: " + fahrenheit);
+    }
+}
