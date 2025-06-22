adcc357540638132722554153877e66d92a61e4e
yuri <97260704+yuri342@users.noreply.github.com>
2025-06-22 14:32:51 -0300

    Update Exercicios-For_while.java

--- Exercicios-For_while.java
+++ Exercicios-For_while.java
@@ -182,3 +182,24 @@ public class ExerciciosCondicionaisMonolito {
         }
     }
 }
+
+
+import java.util.Scanner;
+
+public class SubidaEscadaria {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de degraus: ");
+        int degraus = scanner.nextInt();
+
+        for (int i = 1; i <= degraus; i++) {
+            System.out.println("Subindo o degrau " + i);
+        }
+
+        System.out.println("Você chegou ao topo!");
+
+        scanner.close();
+    }
+}
+
