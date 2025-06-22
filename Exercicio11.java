36e324677fdc20a772c80f2ce87abe6d1979038a
yuri <97260704+yuri342@users.noreply.github.com>
2025-06-22 14:11:29 -0300

    Create Exercicio11.java

---
+++ Exercicio11.java
@@ -0,0 +1,14 @@
+public class DadosPessoais {
+
+    public static void main(String[] args) {
+        String nome = "Maria";
+        int idade = 25;
+        double altura = 1.68;
+        boolean estudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("É estudante: " + estudante);
+    }
+}
