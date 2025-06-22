8e298de64d31617bff086452e8dd8499468c85a9
yuri <97260704+yuri342@users.noreply.github.com>
2025-06-22 14:22:28 -0300

    Create Exercicio18.java

---
+++ Exercicio18.java
@@ -0,0 +1,12 @@
+public class ParOuImpar {
+
+    public static void main(String[] args) {
+        int numero = 7;
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é ímpar.");
+        }
+    }
+}
